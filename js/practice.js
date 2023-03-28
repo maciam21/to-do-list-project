0513df7b421f44bd8c7063ceb85dd7c89307a8ff
Miguel MAcias <maciasflores1990@gmail.com>
2023-03-28 12:34:28 -0700

    first commit

---
+++ js/practice.js
@@ -0,0 +1,9 @@
+document.title="To-Do-List";
+
+const header = document.createElement("header");
+header.style.backgroundColor = "lightblue";
+
+const footer = document.createElement (:"footer");
+footer.classList.add("text-center");
+
+const h1 = document.createElement
